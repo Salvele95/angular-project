85ce68b75f123de4f2a18ed6ef96a621f6a8bb65
Salvele95 <saxthebest95@gmail.com>
2022-07-20 12:56:45 +0200

    3 files modified

--- src/app/components/beer-form/beer-form.component.ts
+++ src/app/components/beer-form/beer-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { allBeerTypes, BeerForm } from '../../model/model';
+import { allBeerTypes, Beer, BeerForm } from '../../model/model';
+import { BeerService } from '../../services/beer.service';
 
 @Component({
   selector: 'app-beer-form',
@@ -11,10 +12,24 @@ export class BeerFormComponent implements OnInit {
   beerTypes = allBeerTypes;
   beerForm: FormGroup<BeerForm>;
 
-  constructor() {}
+  constructor(private beerService: BeerService) {}
 
   ngOnInit() {
     this.beerForm = this.initForm();
+  }
+
+  onSubmit() {
+    const beerFromForm = this.beerForm.value;
+    const beer: Beer = {
+      beerName: beerFromForm.beerName!, //il ! garantisce il beerName
+      beerStyle: beerFromForm.beerStyle!,
+      upc: beerFromForm.upc!,
+      price: beerFromForm.price!,
+      createdDate: new Date(),
+      lastModifiedDate: new Date(),
+      id: this.beerService.generateId(),
+    };
+    this.beerService.addBeer(beer);
   }
 
   private initForm(): FormGroup<BeerForm> {
@@ -22,7 +37,7 @@ export class BeerFormComponent implements OnInit {
       beerName: new FormControl(null, Validators.required),
       beerStyle: new FormControl(null, Validators.required),
       upc: new FormControl(null, Validators.required),
-      price: new FormControl(null, Validators.required),
+      price: new FormControl(null, [Validators.required, Validators.min(0)]),
     });
   }
 }
