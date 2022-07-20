8b8bc7fa6f0634aea7abaeacbe4f5393e8e0c98b
Salvele95 <saxthebest95@gmail.com>
2022-07-20 10:48:58 +0200

    3 files modified

--- src/app/components/beer/beer.component.ts
+++ src/app/components/beer/beer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Beer } from '../../model/model';
+import { BeerService } from '../../services/beer.service';
 
 @Component({
   selector: 'app-beer',
@@ -9,7 +10,11 @@ import { Beer } from '../../model/model';
 export class BeerComponent implements OnInit {
   @Input() beer: Beer;
 
-  constructor() {}
+  constructor(private beerService: BeerService) {}
 
   ngOnInit() {}
+
+  onDelete(beer: Beer) {
+    this.beerService.deleteBeer(beer);
+  }
 }
